fbaed664decdab2f5f70a63ffbe3cf8472161b48
Boluwatife <omotayodaniel10@gmail.com>
2023-10-13 19:21:30 +0100

    feat: navbr responsive

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,30 +1,64 @@
 "use client";
-import React from "react";
-import Button from "./Button";
+import React, { useState } from "react";
+import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import Menu from "@public/assets/menu.png";
+import CloseMenu from "@public/assets/close-menu.png";
 
 const Nav = () => {
   const currentRoute = usePathname();
+
+  const [navOpened, setNavOpened] = useState(false);
+
+  const toggleNav = () => {
+    setNavOpened(!navOpened);
+  };
+
+  const closeNav = () => {
+    setNavOpened(false);
+  };
+
   return (
     <section>
-      <div className="relative lg:px-[8%] px-[4%] py-[2%] bg-dark-purple text-white">
+      <div className="relative lg:px-[8%] px-[4%] md:py-[2%] py-[4%] bg-dark-purple text-white">
         <div className="flex flex-row items-center justify-between">
           <Link href="/">
             <h2>
               get<span className="text-secondary-purple">linked</span>
             </h2>
           </Link>
-          <div className="flex lg:flex-row flex-col lg:items-center items-start gap-16">
+          <div className="lg:hidden block mr-4">
+            <Image
+              src={navOpened ? CloseMenu : Menu}
+              onClick={toggleNav}
+              alt="menu"
+              className="cursor-pointer"
+            />
+          </div>
+          <div
+            className={`lg:flex lg:flex-row lg:items-center gap-8
+          ${
+            navOpened
+              ? "absolute bg-fixed w-full h-screen bg-dark-purple top-16 left-0 z-10 flex items-start flex-col p-10"
+              : "hidden"
+          }`}
+          >
             <ul className="flex lg:flex-row flex-col lg:items-center items-start gap-8">
               <li>
-                <Link href="/#timeline">Timeline</Link>
+                <Link href="/#timeline" onClick={closeNav}>
+                  Timeline
+                </Link>
               </li>
               <li>
-                <Link href="/#overview">Overview</Link>
+                <Link href="/#overview" onClick={closeNav}>
+                  Overview
+                </Link>
               </li>
               <li>
-                <Link href="/#faqs">FAQs</Link>
+                <Link href="/#faqs" onClick={closeNav}>
+                  FAQs
+                </Link>
               </li>
               <li>
                 <Link
@@ -34,6 +68,7 @@ const Nav = () => {
                       ? "text-transparent bg-clip-text bg-gradient-to-r from-primary-purple to-primary-pink"
                       : "text-white"
                   }
+                  onClick={closeNav}
                 >
                   Contact
                 </Link>
@@ -53,6 +88,7 @@ const Nav = () => {
                       ? "bg-dark-purple py-4 px-12"
                       : "bg-gradient-to-r from-primary-purple to-primary-pink rounded-md py-4 px-12 text-base hover:bg-gradient-to-l hover:opacity-80"
                   }
+                  onClick={closeNav}
                 >
                   Register
                 </button>
